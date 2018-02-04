a12cd024ceb8b091c22272247cebbdba9415229d
Sergio Correa <correa.sergio@gmail.com>
2018-02-04 01:54:31 +0000

    Create and migrate styles table, create/models/table.rb

---
+++ db/migrate/20180204014936_create_styles.rb
@@ -0,0 +1,9 @@
+class CreateStyles < ActiveRecord::Migration
+  def change
+    create_table :styles do |t|
+      t.string :name
+      t.string :description
+      t.timestamps null: false
+    end
+  end
+end
