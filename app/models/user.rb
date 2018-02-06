bd1fac95d657ce549bbd56c9911cb0612a505beb
Sergio Correa <correa.sergio@gmail.com>
2018-02-06 03:01:07 +0000

    Add #slug and and #self.find_by_slug User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
 
+  def slug
+    self.username.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
 
 end
