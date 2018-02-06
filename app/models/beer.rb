efead494502e29dbac2dd6c8dd88751c01eaf840
Sergio Correa <correa.sergio@gmail.com>
2018-02-06 02:12:15 +0000

    Add associations and validations to Beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,9 @@
 class Beer < ActiveRecord::Base
+  belongs_to :brewery
+  belongs_to :style
+  has_many :experiences, inverse_of: :beer
+  has_many :users, through: :experiences
+
+  validates_presence_of :brewery, :style, :name
+  validates :abv, presence: true, length: {in: 0..30}
 end
