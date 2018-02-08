a47e0b7837c7d3b0903bcbe23e3e51659a1a5be5
Sergio Correa <correa.sergio@gmail.com>
2018-02-08 01:55:09 +0000

    Add to ApplicationController helper methods #logged_in?, #current_user, and #experience_ownership?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,19 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+
+    def experience_ownership?(experience)
+      current_user == experience.user
+    end
+  end
+
+
 end
