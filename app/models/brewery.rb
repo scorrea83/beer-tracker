5fc91906c93a616ef3dc07adb3ad7de9898d397e
Sergio Correa <correa.sergio@gmail.com>
2018-02-10 20:30:56 +0000

    Add #slug and .find_by_slug methods to Brewery class

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,4 +4,12 @@ class Brewery < ActiveRecord::Base
 
   validates_presence_of :name, :country
 
+  def slug
+    self.username.downcase.split(" ").join("-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
 end
