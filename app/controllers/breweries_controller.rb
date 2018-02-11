7d51c7352201f77f1466332938f9a6e661c9de1f
Sergio Correa <correa.sergio@gmail.com>
2018-02-11 00:48:35 +0000

    Add GET '/breweries/:slug' controller action

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,5 +4,9 @@ class BreweriesController < ApplicationController
       erb :'breweries/breweries'
   end
 
+  get '/breweries/:slug' do
+    @brewery = Brewery.find_by_slug(params[:slug])
+    erb :'breweries/show_brewery'
+  end
 
 end
