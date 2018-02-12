60bef88457ba15836e72a54094e76527605ec560
Sergio Correa <correa.sergio@gmail.com>
2018-02-12 20:17:15 +0000

    Add GET '/experiences/:id' controller action

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,4 +3,9 @@ class ExperiencesController < ApplicationController
   get '/experiences' do
     erb :'experiences/experiences'
   end
+
+  get '/experiences/:id' do
+    @experience = Experience.find(params[:id])
+    erb :'experiences/show_experience'
+  end
 end
