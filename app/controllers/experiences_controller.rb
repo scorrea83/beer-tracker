49aff165daf298589be24c62d07a532b5bfa7b38
Sergio Correa <correa.sergio@gmail.com>
2018-02-18 01:48:45 +0000

    Add form to select brewery from list

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -7,6 +7,11 @@ class ExperiencesController < ApplicationController
     erb :'experiences/experiences'
   end
 
+  get '/experiences/new' do
+    erb :'experiences/create_experience'
+  end
+
+
   get '/experiences/:id' do
     @experience = Experience.find(params[:id])
     erb :'experiences/show_experience'
